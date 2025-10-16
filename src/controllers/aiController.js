ff88ff7748c4f3c5e83730851d052421ad69ea3b
fresendiz <fresendiz@fabricadesoluciones.com>
2025-10-16 14:17:00 -0600

    CRUD facturas

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -3,7 +3,8 @@
 const geminiService = require('../services/geminiService');
 const multer = require('multer');
 const fs = require('fs'); 
-const path = require('path'); // <-- Necesario para la ruta absoluta
+const path = require('path'); 
+const { Client } = require('../models');
 
 // --- Configuración de Multer (Almacenamiento en Disco) ---
 const diskStorage = multer.diskStorage({
@@ -46,11 +47,50 @@ const analyzePdfRoute = async (req, res) => {
             file.originalname,
             question
         );
+
+        const rawResponseString = generatedResponse;
+        let datosFactura = null;
+        let nombreCliente = null;
+        let clienteEncontrado = null;
+        let total = null;
+        
+        try {
+            // Limpieza y Parseo del JSON anidado
+            const jsonStringLimpia = rawResponseString
+                .replace('```json\n', '')
+                .replace('\n```', '')
+                .trim();
+
+            datosFactura = JSON.parse(jsonStringLimpia);
+            nombreCliente = datosFactura.cliente;
+            total = datosFactura.total_monto
+            // 2. Búsqueda en la Base de Datos
+            if (nombreCliente) {
+                // Asume que Cliente.findOne() es una función de tu ORM/ODM
+                // Usa un try/catch específico para esta búsqueda si es necesario
+                const nombreLimpio = nombreCliente.trim(); 
+                clienteEncontrado = await Client.findOne({
+                    where: {
+                        name: nombreLimpio // Si esto trae resultados correctos, el problema es el Op/fn
+                    }
+                });
+            }
+            
+        } catch (parseError) {
+            return res.status(202).json({ 
+                code: 2, 
+                message: "IA respondió, pero el procesamiento de datos falló.",
+                raw_response: generatedResponse,
+                file_name: file.originalname
+            });
+        }
         
         res.status(200).json({
             code: 1,
+            client: clienteEncontrado,
             response: generatedResponse,
-            file_name: file.originalname 
+            file_name: file.originalname,
+            total: total
         });
 
     } catch (error) {
@@ -58,10 +98,10 @@ const analyzePdfRoute = async (req, res) => {
         res.status(500).json({ code: 0, error: error.message || "Fallo en el servicio de análisis." });
     } finally {
         // 2. Limpieza del archivo local del disco
-        // if (filePath && fs.existsSync(filePath)) {
-        //     fs.unlinkSync(filePath);
-        //     console.log(`[MULTER] Archivo local eliminado: ${filePath}`);
-        // }
+        if (filePath && fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+            //console.log(`[MULTER] Archivo local eliminado: ${filePath}`);
+        }
     }
 };
 
